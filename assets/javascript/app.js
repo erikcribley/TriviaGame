8e59611f6a73e8c66b89b54e18b0918001ed53e6
Erik Cribley <49967226+erikcribley@users.noreply.github.com>
2019-06-10 14:57:13 -0400

    questions and answers displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,49 @@
-//question array
-const questions = [
-    "Question 1",
-    "Question 2",
-    "Question 3",
-    "Question 4",
-    "Question 5"
-]
+//Question&Answer Objects
+const questions = {
+    q1: "Question1",
+    q2: "Question2",
+    q3: "Question3",
+    q4: "Question4",
+    q5: "Question5"
+}
 
-//global variable
+const options = {
+    q1: ["Answer1", "Answer2", "Answer3", "Answer4"],
+    q2: ["Answer1", "Answer2", "Answer3", "Answer4"],
+    q3: ["Answer1", "Answer2", "Answer3", "Answer4"],
+    q4: ["Answer1", "Answer2", "Answer3", "Answer4"],
+    q5: ["Answer1", "Answer2", "Answer3", "Answer4"],
+}
+
+const correctAnswer = {
+    q1: "Answer1",
+    q2: "Answer3",
+    q3: "Answer2",
+    q4: "Answer2",
+    q5: "Answer1"
+}
+
+//global variables
 let correct = 0;
 let incorrect = 0;
 let unanswered = 0;
 let time = 30;
 
-function timer (){
+//countdown
+function timer () {
     time--;
     $('#timer-display').text(time + ' Seconds Remaining')
 }
 
-function nextQuestion (){
-
-}
-
-function answer (){
-
-}
-
+//quiz
 function quiz () {
     setInterval(timer, 1000)
     $('#timer-display').text(time + ' Seconds Remaining')
-    $('#question-display').text(questions[0]);
-    $('#answers-display')
+    $('#question-display').text(questions.q1)
+    for (i = 0; i < options.q1.length; i++) {
+        $('#answers-display').append($('<button>' + options.q1[i] + '</button>')).addClass()
+    }
+
 }
 
 // start button
