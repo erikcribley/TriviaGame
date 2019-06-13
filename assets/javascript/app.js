1c5adc313159458b44f0bf3d6308850c93e2d2cd
Erik Cribley <49967226+erikcribley@users.noreply.github.com>
2019-06-13 02:36:00 -0400

    game over and reset function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -60,7 +60,17 @@ function timer () {
 }
 
 function gameOver() {
-
+    $('#question-display')
+        .text("GAME OVER")
+    $('.answer-btn')
+        .hide()
+    $('#answer-info')
+        .empty()
+        .append('<p>', "Correct " + correct)
+        .append('<p>', "Incorrect " + incorrect)
+        .append('<p>', "Time Up " + unanswered)
+        .append("<p> Play Again? </p>")
+    $('#start').show();  
 }
 
 //function to check answer and display results
@@ -94,68 +104,74 @@ function isCorrect(val, num) {
             .text(questions[num].info)
     }
     let newNum = num += 1 
-    setTimeout(quiz, 3000, newNum)
+    if (newNum === 5) {
+        gameOver()
+    } else {
+    setTimeout(quiz, 5000, newNum)
+    }
 }
+
 
 //quiz
 function quiz (num) {
-    if (num === 5) {
-        gameOver()
-    } else {
-        $('#answer-info')
-            .hide()
-        $('.answer-btn')
-            .show()
-        time = 10
-        let countDown = setInterval(timer, 1000);
-        $('#timer-display')
-            .text(time + ' Seconds Remaining')
-        $('#question-display')
-            .text(questions[num].question)
-        $('#answer-1')
-            .text(questions[num].a)
-        $('#answer-2')
-            .text(questions[num].b)
-        $('#answer-3')
-            .text(questions[num].c)
-        $('#answer-4')
-            .text(questions[num].d)
-    
-        let timeUp = setTimeout(isCorrect, 10000, "timeout", num)
 
-        //Click events for answer buttons
-        $('#answer-1').on("click", function (){
-            let userGuess = $('#answer-1').text()
-            clearInterval(countDown);
-            clearTimeout(timeUp);
-            isCorrect(userGuess, num);
-        })
-        $('#answer-2').on("click", function (){
-            let userGuess = $('#answer-2').text()
-            clearInterval(countDown);
-            clearTimeout(timeUp);
-            isCorrect(userGuess, num);
-        })
-        $('#answer-3').on("click", function (){
-            let userGuess = $('#answer-3').text()
-            clearInterval(countDown)
-            clearTimeout(timeUp);
-            isCorrect(userGuess, num);
-        })
-        $('#answer-4').on("click", function (){
-            let userGuess = $('#answer-4').text()
-            clearInterval(countDown);
-            clearTimeout(timeUp);
-            isCorrect(userGuess, num);
-        })
-    }
+    $('#answer-info')
+        .empty()
+    $('.answer-btn')
+        .show()
+    time = 10
+    let countDown = setInterval(timer, 1000);
+    $('#timer-display')
+        .text(time + ' Seconds Remaining')
+    $('#question-display')
+        .text(questions[num].question)
+    $('#answer-1')
+        .text(questions[num].a)
+    $('#answer-2')
+        .text(questions[num].b)
+    $('#answer-3')
+        .text(questions[num].c)
+    $('#answer-4')
+        .text(questions[num].d)
+    
+    let timeUp = setTimeout(isCorrect, 10000, "timeout", num)
+
+    //Click events for answer buttons
+    $('#answer-1').on("click", function (){
+        let userGuess = $('#answer-1').text()
+        clearInterval(countDown);
+        clearTimeout(timeUp);
+        isCorrect(userGuess, num);
+    })
+    $('#answer-2').on("click", function (){
+        let userGuess = $('#answer-2').text()
+        clearInterval(countDown);
+        clearTimeout(timeUp);
+        isCorrect(userGuess, num);
+    })
+    $('#answer-3').on("click", function (){
+        let userGuess = $('#answer-3').text()
+        clearInterval(countDown)
+        clearTimeout(timeUp);
+        isCorrect(userGuess, num);
+    })
+    $('#answer-4').on("click", function (){
+        let userGuess = $('#answer-4').text()
+        clearInterval(countDown);
+        clearTimeout(timeUp);
+        isCorrect(userGuess, num);
+    })
 }
 
 //hide answer buttons
 $('.answer-btn').hide()
 
-//start button
+//start & reset button
 $('#start').on("click", function () {
     $('#start').hide();
-    quiz(0)    
+    let correct = 0;
+    let incorrect = 0;
+    let unanswered = 0;
+    let time = 10;
+    quiz(0) 
 });
